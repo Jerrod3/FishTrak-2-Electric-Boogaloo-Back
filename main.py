3102086c26d88d8b6be48ba8b601d25e34c71681
lucas <lucas.pjensen10@gmail.com>
2022-09-13 17:29:35 -0700

    continued work on tests and yaml file

--- main.py
+++ main.py
@@ -1,27 +1,27 @@
 import uvicorn
-# import os
+import os
 from fastapi import FastAPI, APIRouter
 from pymongo import MongoClient
 from routes.fishermen import router as fishermen_router
 from routes.lures import router as lures_router
 from routes.water_body import router as water_bodies_router
 from routes.species import router as species_router
-# from dotenv import dotenv_values
+from dotenv import dotenv_values
 
 
 # set environmental variables based on location of Secrets
-# if os.path.exists('.env'):
-#     print("reading from .env file!")
-#     config = dotenv_values(".env")
-#     ATLAS_URI = config["ATLAS_URI"]
-#     ATLAS_DB = config["DB_NAME"]
-# else:
-#     print("No .env file found. Hopefully the variables exist!")
-#     ATLAS_URI = os.getenv("ATLAS_URI")
-#     ATLAS_DB = os.getenv("DB_NAME")
+if os.path.exists('.env'):
+    print("reading from .env file!")
+    config = dotenv_values(".env")
+    ATLAS_URI = config["ATLAS_URI"]
+    ATLAS_DB = config["DB_NAME"]
+else:
+    print("No .env file found. Hopefully the variables exist!")
+    ATLAS_URI = os.getenv("ATLAS_URI")
+    ATLAS_DB = os.getenv("DB_NAME")
 
-ATLAS_URI = "mongodb+srv://FishTrak:GulxviwQcpGpk7Xe@cluster0.1kofm.mongodb.net/?retryWrites=true&w=majority"
-ATLAS_DB = "FishTrak"
+# ATLAS_URI = "mongodb+srv://FishTrak:GulxviwQcpGpk7Xe@cluster0.1kofm.mongodb.net/?retryWrites=true&w=majority"
+# ATLAS_DB = "FishTrak"
 
 app = FastAPI()
 
